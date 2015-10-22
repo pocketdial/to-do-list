0b82820706f75a446872abd4a77b78cc01324dda
DK <qmitry@gmail.com>
2015-10-22 23:18:20 +0300

    Added new routes and actions

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,8 +8,20 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        //return ok(index.render("Your new application is ready."));
-        return ok("Test Heroku :)");
+        return redirect(routes.Application.tasks());
+    }
+
+    public static Result tasks() {
+        return TODO;
+    }
+
+    public static Result newTask() {
+        return TODO;
+    }
+
+    public static Result deleteTask(Long id) {
+        return TODO;
     }
 
 }
+
