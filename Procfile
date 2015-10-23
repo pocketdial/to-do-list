014e1201ccca79664c0eb616ff7249ae011da30f
DK <qmitry@gmail.com>
2015-10-24 00:19:20 +0300

    Added Task model, forms, etc

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: target/universal/stage/bin/play-todo-postgres -Dhttp.port=$PORT -DapplyEvolutions.default=true
--- app/models/Task.java
+++ app/models/Task.java
@@ -1,23 +1,33 @@
 package models;
 
 import java.util.*;
+import play.db.ebean.*;
 import play.data.validation.Constraints.*;
+import javax.persistence.*;
 
-public class Task {
+@Entity
+public class Task extends Model {
 
+    @Id
     public Long id;
 
     @Required
     public String label;
 
+    public static Finder<Long,Task> find = new Finder(
+            Long.class, Task.class
+    );
+
     public static List<Task> all() {
-        return new ArrayList<Task>();
+        return find.all();
     }
 
     public static void create(Task task) {
+        task.save();
     }
 
     public static void delete(Long id) {
+        find.ref(id).delete();
     }
 
 }
